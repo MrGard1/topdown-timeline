e151700c28b8fc53b81c4760e00a9176fd3ae374
Matthew Gardiner <matthew.gardiner@washk12.org>
2024-12-10 09:25:52 -0700

    rm old mistnamesd

--- gameLoop.py
+++
@@ -1,8 +0,0 @@
-def play(game_settings, starting_game_table ):
-    print("having lots of fun playing timeline")
-    # current_player = 1
-    # while not game_over():
-    #     take_turn(current_player)
-    #     current_player = pick_next_player()
-    player_hands = starting_game_table["player_hands"]
-    return player_hands
